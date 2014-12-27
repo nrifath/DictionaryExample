5866de9ed8c7ae35450c35afb16b30c10065d70f
nrifath <nrifath@yahoo.com>
2014-12-27 20:30:23 +0600

    New Project Added

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> courses = new Dictionary<string, string>();
+            courses.Add("cse-001","Computer Fundamental");
+            courses.Add("cse-002", "Structured Programming");
+
+
+            foreach (var course in courses)
+            {
+                Console.Write(course.Key);
+                Console.Write(": ");
+                Console.WriteLine(course.Value);
+            }
+
+            Console.WriteLine("Enter Course Code: ");
+            string courseCode = Console.ReadLine();
+            Console.WriteLine("Enter Course Title");
+            string courseTitle = Console.ReadLine();
+
+            if (courses.ContainsKey(courseCode))
+            {
+                Console.WriteLine("Course Code Already Exist");
+            }
+            else
+            {
+                courses.Add(courseCode,courseTitle);
+                Console.WriteLine("Course Inserted Successfully");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
